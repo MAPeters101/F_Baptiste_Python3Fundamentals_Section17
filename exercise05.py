52cff9adda17cc107a28396316e2da362d8ec4c0
Mark Peters <mapeters101@earthlink.net>
2025-03-10 23:24:08 -0400

    Descriptions for exercise05.

---
+++ exercise05.py
@@ -0,0 +1,10 @@
+'''
+Question 5
+Given an arbitrary list of numbers, write an expression that returns the
+smallest value in the list based on the absolute values of ech number.
+
+For example, given the list:
+
+l = [5, 6, -4, 8]
+Your expression should return -4.
+'''
