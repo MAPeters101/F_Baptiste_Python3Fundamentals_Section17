992a3c841d073840c0a6e55b077b7fe51d45efdd
Mark Peters <mapeters101@earthlink.net>
2025-03-10 23:20:24 -0400

    Descriptions for exercise01.

---
+++ exercise01.py
@@ -0,0 +1,23 @@
+'''
+Question 1
+Given a list of tuples containing two numerical values, write a function that
+returns a list of the same tuples, sorted by the absolute value of the
+difference between the two numbers, in descending order.
+
+For example, if the input is:
+
+l = [
+    (1, 2),
+    (-4, -5.5),
+    (10, -10),
+    (-2, 2)
+]
+Then the return value of the function should be:
+
+[
+  (10, -10)
+  (-2, 2),
+  (-4, -5.5),
+  (1, 2)
+]
+'''
