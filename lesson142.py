1b3b0e9a4d8caede35840ae146e916a088cba716
Mark Peters <mapeters101@earthlink.net>
2025-03-19 23:35:05 -0400

    Work on lesson142 from 2:10 to 3:10.

--- lesson142.py
+++ lesson142.py
@@ -11,3 +11,11 @@ print('-'*80)
 evens = (n for n in data if is_even(n))
 print(list(evens))
 print(list(evens))
+print('-'*80)
+
+evens = filter(is_even, data)
+print(evens)
+print(list(evens))
+print(list(evens))
+
+
