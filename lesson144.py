287b8a007b93e1c08d5469141c7cd3dc011fb172
Mark Peters <mapeters101@earthlink.net>
2025-03-20 17:16:45 -0400

    Work on lesson144 from 1:00 to 3:30.

--- lesson144.py
+++ lesson144.py
@@ -1,5 +1,17 @@
 print(1 < 2)
 print('A' < 'Z')
 
+def key_func(x):
+    return abs(x)
+
+data = [-10, -6, 0, 3, 6]
+
+print(sorted(data))
+
+print([key_func(x) for x in data])
+
+print(sorted(data, key=key_func))
+print('-'*80)
+
 
 
