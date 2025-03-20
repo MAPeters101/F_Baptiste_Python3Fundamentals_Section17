04cca4e499ca92b228fe5818425a326466263ae7
Mark Peters <mapeters101@earthlink.net>
2025-03-20 17:30:24 -0400

    Work on lesson144 from 5:25 to 7:40.

--- lesson144.py
+++ lesson144.py
@@ -21,6 +21,19 @@ print('-'*80)
 print(sorted(data, key=lambda x: abs(x)))
 print(sorted(data, key=abs))
 print('-'*80)
+from pprint import pprint
+data = [
+    {'date': '2020-04-09', 'symbol': 'AAPL', 'open': 268.70, 'high': 270.04, 'low': 264.70, 'close': 267.99},
+    {'date': '2020-04-09', 'symbol': 'MSFT', 'open': 166.36, 'high': 167.37, 'low': 163.33, 'close': 165.14},
+    {'date': '2020-04-09', 'symbol': 'AMZN', 'open': 2_044.30, 'high': 2_053.00, 'low': 2_017.66, 'close': 2_042.76},
+    {'date': '2020-04-09', 'symbol': 'FB', 'open': 175.90, 'high': 177.08, 'low': 171.57, 'close': 175.19}
+]
+print(sorted(data, key=lambda d: d['symbol']))
+pprint(sorted(data, key=lambda d: d['symbol']))
+
+print(sorted(data, key=lambda d: d['close'], reverse=True))
+pprint(sorted(data, key=lambda d: d['close'], reverse=True))
+print('-'*80)
 
 
 
